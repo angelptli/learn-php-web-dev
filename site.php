cbf379032d5ddac0a94fd032e5cc5f93beea2513
angelptli <ali95@mail.ccsf.edu>
2022-04-14 13:06:35 -0700

    Echo html tagged text in php

--- site.php
+++ site.php
@@ -2,12 +2,20 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>
+      Hello PHP Server
+    </title>
   </head>
 
   <body>
     <?php 
-      echo("Hello PHP Server");
-     ?>
+      echo
+      "<center>
+        <h1>Header 1</h1>
+        <h2>Header 2</h2>
+        <hr>
+        <p>Hello PHP Server!</p>
+      </center>";
+    ?>
   </body>
 </html>
