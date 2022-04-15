aab67ea755f3610b9191536b16815419df8916f3
angelptli <ali95@mail.ccsf.edu>
2022-04-14 19:52:36 -0700

    Use array elements to display in a sentence

---
+++ 05_arrays/arrays.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>
+      Arrays
+    </title>
+    
+  </head>
+
+  <body>
+    <?php
+      $fruits = array("grapefruit", "grape", "starfruit", "orange", "durian");
+      $items = array("wallet", 3, true);
+
+      echo "I have " . $items[1] . " " . $fruits[2] . "s. <br>";
+      echo "I  have " . count($fruits) . " fruits. <br>";
+
+    ?>
+  </body>
+</html>
