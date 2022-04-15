b70e9589e524f194a0dbf2b5f86814ea6fe5f8a0
angelptli <ali95@mail.ccsf.edu>
2022-04-14 21:40:53 -0700

    Upgrade calculator to take in decimals and allow addition, subtraction, division, modulo division, and multiplication

--- 02_calculator/calculator.php
+++ 02_calculator/calculator.php
@@ -8,21 +8,39 @@
   </head>
 
   <body>
-    <form action="calculator.php" method="get">
-      <input type="number" name="input_num1">
-      <br>
-      <input type="number" name="input_num2">
+    <form action="calculator.php" method="post">
+      Enter a number: <input type="number" step="0.1" name="input_num1"><br>
+      Enter a sign: <input type="textbox" step="0.1" name="operator"><br>
+      Enter a number: <input type="number" name="input_num2"><br>
       <input type="submit">
     </form>
 
     <br>
     
     <?php
-      if(isset($_GET["input_num1"]) and isset($_GET["input_num2"])) {
-        echo $_GET["input_num1"] . " + " . $_GET["input_num2"] . " = ";
-        echo $_GET["input_num1"] + $_GET["input_num2"];
-      } else {
-        echo "Enter a number into each field <br>";
+      $input_num1 = $_POST["input_num1"] ?? 0;
+      $input_num2 = $_POST["input_num2"] ?? 0;
+      $operator = $_POST["operator"] ?? "";
+      $result = 0;
+
+      if ($operator == "+")
+        $result = $input_num1 + $input_num2;
+      elseif ($operator == "-")
+        $result = $input_num1 - $input_num2;
+      elseif ($operator == "/")
+        $result = $input_num1 / $input_num2;
+      elseif ($operator == "%")
+        $result = $input_num1 % $input_num2;
+      elseif ($operator == "*")
+        $result = $input_num1 * $input_num2;
+      elseif ($operator == "")
+        $result = 0;
+      else
+        echo "Invalid operator.<br>";
+
+      if(isset($_POST["input_num1"]) && isset($_POST["input_num2"]) && isset($_POST["operator"])) {
+        echo $_POST["input_num1"] . " " . $_POST["operator"] . " " . $_POST["input_num2"] . " = ";
+        echo $result;
       }
     ?>
   </body>
