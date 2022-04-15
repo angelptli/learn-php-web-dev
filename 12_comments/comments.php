51b8feb1282b2dea802bce044ce66c7a78675c70
angelptli <ali95@mail.ccsf.edu>
2022-04-14 22:31:27 -0700

    Use different php comment methods

---
+++ 12_comments/comments.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>
+      Just Comments
+    </title>
+  </head>
+  <body>
+    <?php
+      // Single Line Comment
+
+      /*
+        This
+        is
+        a
+        multiline
+        comment
+      */
+
+    ?>
+  </body>
+</html>
